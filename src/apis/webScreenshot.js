9d5961498f1d0da7fffebbb985a03b4bdb767f9c
Henry Park <hanry.park3@gmail.com>
2020-06-08 12:40:48 -0700

    convert png file to base64

--- src/apis/webScreenshot.js
+++ src/apis/webScreenshot.js
@@ -1,12 +1,15 @@
 import puppeteer from "puppeteer";
 
-async function webScreenshot(websiteURL) {
+async function webScreenshot( websiteURL ) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(websiteURL);
-    await page.screenshot({path: `${websiteURL.substring(8)}.png`});
+    await page.goto( websiteURL );
+    const imagePath = `${ websiteURL.substring(8) }.png`;
+    await page.screenshot({ path: imagePath });
 
     await browser.close();
+
+    return imagePath;
 }
 
 export default webScreenshot;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import bodyParser from "body-parser"
 import webScreenshot from "./apis/webScreenshot"
+import imageAsBase64 from "./apis/imageAsBase64"
 
 const app = express();
 const port = 3000;
@@ -8,8 +9,10 @@ const port = 3000;
 app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
-    webScreenshot(req.body.url);
-    res.status(200).send('successful screenshot');
+    const filePath = webScreenshot(req.body.url);
+    filePath.then( url => imageAsBase64(url) ).then(
+        base64Image => res.status(200).send(base64Image)
+    );
 });
 
 
